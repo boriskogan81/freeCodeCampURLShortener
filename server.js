6b988907ee228b308fc811a9109e1e980d269f18
Boris (Baruch) Kogan <boris.kogan81@gmail.com>
2016-07-26 20:37:21 +0000

    added shorturl in npm, getting cannot GET error

--- server.js
+++ server.js
@@ -1,80 +1,71 @@
 var express = require("express");
 var mongo = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 var app = express();
-var url = 'mongodb://localhost:27017/urls';
-var count = 0;
-function counts(err, x){
-  if (err)throw err;
-  return parseInt(x.count());
-}
+var mongourl = 'mongodb://localhost:27017/urls';
+var favicon = require('serve-favicon');
+var shortid = require('shortid');
 
-;
-//this is what we will use to increment the new URL number
-function findUrl(x) {mongo.connect(url, function(err, db) { //this function looks up original URLs to see if they're already in our db
-  console.log('finding ' + x + ' in the database');
-  if (err) {
-    console.error('There was an error finding ' + x + ' in the database!', err);
-    return;
-  }
-  var urls = db.collection('urls');
-  count = counts(urls);
-  console.log(count + ' entries in the url database');
-  //every time we look up a new original URL, we will update the count to the length of the 'urls' collection
-  urls.find({originalUrl:'x'
-  }).toArray(function(err, docs) {
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+//this is what we will use to find the URL
+function findUrl(url) {
+  mongo.connect(mongourl, function(err, db) {
     if (err) {
-    console.error('There was an error returning the urls found in the database!', err);
-    return;
+    console.error('There was an error', err);
+    return false;
     }
-    db.close();
-    return docs;
+    //this function looks up original URLs to see if they're already in our db
+    var urls = db.collection('urls');
+    urls.find({ shorturl: url }, function(err, docs) {
+        if (err) {
+          return 'There was an error returning the urls found in the database!';
+        }
+        db.close();
+        return docs;
+      });
     });
-});
 }
 
-function insertURL(x){mongo.connect(url, function(err, db) { //this function inserts new original
+function insertURL(url){mongo.connect(url, function(err, db) { //this function inserts new original
   if (err) {
     console.error('There was an error setting up the insertURL connection!', err);
     return;
   }
-  console.log('inserting ' + x + ' into the database');
+  console.log('inserting ' + url + ' into the database');
   var urls = db.collection('urls');
-  urls.insert(x, function(err, data) {
+  urls.insert(url, function(err, data) {
     if (err) {
     console.error('There was an error running the insertURL function!', err);
     return;
   }
-    console.log('inserted ' + JSON.stringify(x) + 'into the url database');
+    console.log('inserted ' + JSON.stringify(url) + 'into the url database');
     db.close();
   });
 });
 }
 
-var sendObj = {'originalUrl':null, 'newUrl':null};
 
-app.get('/new/:url*', function (req, res) {
-  var str = req.url;
-  str = str.slice(5);
-  console.log((str));
-  sendObj.originalUrl = str;
-  if (findUrl(str)){
-    sendObj.newUrl = findUrl(str).newUrl;
-    console.log('url exists in db');
+app.post('/new/', jsonParser, function (req, res) {
+  var original = req.body.original;
+  var result = {};
+  result.original = original;
+  result.shorturl = shortid.generate();
+  if (insertURL(result)) {
+    res.send(result);
+  } else {
+    res.status(400).send(result);
   }
-  else
-  {
-    sendObj.newUrl = parseInt(req.baseUrl + (count + 1));
-    insertURL(sendObj);
-  }
-  res.send(sendObj);
+
 });
 
-app.get(('/:url'), function (req, res) {
+app.get('/:url', function (req, res) {
   var str = req.params.url;
   console.log((str));
   if (findUrl(str)){
     console.log('urlString found, redirecting');
-    res.redirect(findUrl(str).originalUrl);
+    res.redirect(findUrl(str).original);
   }
   else
   {
